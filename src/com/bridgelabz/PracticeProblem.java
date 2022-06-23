bb5ca590b154e9cf6ecf0eb490360357bb464654
Ashribad9871 <ashi19996@gmail.com>
2022-06-23 16:11:33 +0530

    cgpa calculate

---
+++ src/com/bridgelabz/PracticeProblem.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PracticeProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int number = sc.nextInt();
+        System.out.println(number);
+        int a = 9;
+        int b = 4;
+        int c = 7;
+        int sum = a +b+c;
+        System.out.println(sum);
+//      Question2
+    float subject1 = 34;
+    float subject2 = 35;
+    float subject3 = 65;
+    float cgpa = (subject1 + subject3+ subject2)/30;
+        System.out.println(cgpa);
+//      Question3
+        System.out.println("have a good day");
+        String name = sc.next();
+//      Question4
+        System.out.println("enter a number");
+
+
+    }
+}
